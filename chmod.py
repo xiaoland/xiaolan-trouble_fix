36eec8981d427c599ca62dbe2e327c3d763120ae
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-04-21 23:50:23 +0800

    Create chmod.py

---
+++ chmod.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import json
+import requests
+
+os.system('su root')
+os.system('chmod 777 /home/pi/xiaolan-trouble_fix/xl-pyaudio-err[-9985].sh')
+os.system('/home/pi/xiaolan-trouble_fix/xl-pyaudio-err[-9984].sh')
